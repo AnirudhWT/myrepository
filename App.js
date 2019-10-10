b7a7ba50e01a3b845561984b051afe6d8661de01
AnirudhWT <55124564+AnirudhWT@users.noreply.github.com>
2019-10-10 19:57:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Rack from './Rack';
+
+import './styles.css';
+function App() {
+  return (
+
+    <div className="App">
+     <Rack/>
+     
+    </div>
+
+    );
+}
+
+export default App;
